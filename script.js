6fc0b920a883b6e678cc53e6f6cc9f87cf82a5d2
Breezite <mifsudjulian007@gmail.com>
2026-04-14 09:24:43 +0200

    API and Node init

---
+++ script.js
@@ -0,0 +1,23 @@
+async function loadFighters(){
+    try{
+        const response = await fetch('/api/figthers')
+        const data = await response.json()
+
+        const table = document.getElementById('fightersTable')
+        table.innerHTML=''
+
+        data.items.forEach(fighter =>{
+            const row = document.createElement('tr')
+            row.innerHTML=`
+                <td>${fighter.ranking ?? '-'}</td>
+                <td>${fighter.fullName}</td>
+                <td>${fighter.country}</td>
+            `
+            table.appendChild(row)
+        })
+    }
+    catch (error){
+        console.error('Error occured: ', error)
+    }
+}
+loadFighters()
---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import fetch from 'node-fetch'
+import path from 'path'
+
+const app = express()
+const PORT = 3000
+app.use(express.static('public'))
+
+app.get('/api/fighters', async (req, res) =>{
+    try{
+        const response = await fetch('https://public-api.karate.com/api/fighters?limit=20')
+        const data = await response.json()
+
+        res.json(data)
+    }
+    catch (error){
+        res.status(500).json({error: 'Failed to find athletes'})
+    }
+})
+
+app.listen(PORT, () =>{
+    console.log('Server running at http://localhost:${PORT}')
+})
