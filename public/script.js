75b7afda8db39d4d7037b3e00a41acd95c165409
Breezite <mifsudjulian007@gmail.com>
2026-04-22 12:57:00 +0200

    Test scripts

--- public/script.js
+++ public/script.js
@@ -1,61 +1,76 @@
+let allFighters = []; 
+
 async function loadFighters(){
-    const container = document.getElementById('tablesContainer')
-    container.innerHTML='<p>Loading athletes...</p>'
-    try{
+    const container = document.getElementById('tablesContainer');
+    container.innerHTML = '<p>Loading athletes...</p>';
+    
+    try {
         const response = await fetch('/api/fighters');
         const data = await response.json();
-        console.log(data);
-        const filtered = data.items.filter(fighter =>{
-            return fighter.category && fighter.category.includes("KARATE COMBAT");
-        });
-        const grouped={};
-        filtered.forEach(fighter =>{
-            const weight = fighter.weightClass;
-            if(!grouped[weight]){
-                grouped[weight]=[]
-            }
-            grouped[weight].push(fighter);
-        });
-
-        container.innerHTML='';
-
-        for(const weightClass in grouped){
-            const fighterofClass = grouped[weightClass];
-
-            const title = document.createElement('h2');
-            title.textContent = weightClass;
-            container.appendChild(title);
-
-            const table = document.createElement('table');
-            table.innerHTML=`
-                <thead>
-                    <tr>
-                        <th>Rank</th>
-                        <th>Name</th>
-                        <th>Weight</th>
-                    </tr>
-                </thead>
-                <tbody></tbody>
-            `;
-            const tbody = table.querySelector('tbody');
-
-            fighterofClass.forEach(fighter => {
-                const row=document.createElement('tr');
-                row.innerHTML= `
-                    <td>${fighter.ranking ?? '-'}</td>
-                    <td>${fighter.fullName}</td>
-                    <td>${fighter.weightClass}</td>
-                `;
-                tbody.appendChild(row);
-            });
-            container.appendChild(table);
-        }
-        if(Object.keys(grouped).length ===0){
-            container.innerHTML='<p>No athletes</p>'
-        } 
-    }catch (error){
+        
+        allFighters = data.items; 
+        
+        filterFighters('KARATE COMBAT'); 
+        
+    } catch (error) {
         console.log(error);
+        container.innerHTML = '<p>Error loading athletes.</p>';
     }
+}
+
+
+function filterFighters(selectedCategory) {
+    const container = document.getElementById('tablesContainer');
+    container.innerHTML = '';
+
+    const filtered = allFighters.filter(fighter => {
+        return fighter.category && fighter.category.includes(selectedCategory);
+    });
+
+    const grouped = {};
+    filtered.forEach(fighter => {
+        const weight = fighter.weightClass;
+        if (!grouped[weight]) {
+            grouped[weight] = [];
+        }
+        grouped[weight].push(fighter);
+    });
+
+    for (const weightClass in grouped) {
+        const fighterofClass = grouped[weightClass];
+
+        const title = document.createElement('h2');
+        title.textContent = weightClass;
+        container.appendChild(title);
+
+        const table = document.createElement('table');
+        table.innerHTML = `
+            <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Name</th>
+                    <th>Weight</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        `;
+        const tbody = table.querySelector('tbody');
+
+        fighterofClass.forEach(fighter => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${fighter.ranking ?? '-'}</td>
+                <td>${fighter.fullName}</td>
+                <td>${fighter.weightClass}</td>
+            `;
+            tbody.appendChild(row);
+        });
+        container.appendChild(table);
+    }
+
+    if (Object.keys(grouped).length === 0) {
+        container.innerHTML = `<p>No athletes found for ${selectedCategory}</p>`;
+    } 
 }
 
 document.addEventListener('DOMContentLoaded', loadFighters);
