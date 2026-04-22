4dcdf3bb71459f8b5ae8cbf15d361dc83c8034b0
Breezite <mifsudjulian007@gmail.com>
2026-04-22 11:54:14 +0200

    Better rankings

--- public/script.js
+++ public/script.js
@@ -1,25 +1,61 @@
 async function loadFighters(){
-    const table = document.getElementById('fightersTable');
-
-    table.innerHTML='<tr><td>Boo</td></tr>';
+    const container = document.getElementById('tablesContainer')
+    container.innerHTML='<p>Loading athletes...</p>'
     try{
         const response = await fetch('/api/fighters');
         const data = await response.json();
-        console.log("Succesfully fetched: ", data);
-        table.innerHTML='';
-        data.items.forEach(fighter =>{
-            const row = document.createElement('tr')
-            row.innerHTML=`
-                <td>${fighter.ranking ?? '-'}</td>
-                <td>${fighter.fullName}</td>
-                <td>${fighter.country}</td>
-            `;
-            table.appendChild(row);
+        console.log(data);
+        const filtered = data.items.filter(fighter =>{
+            return fighter.category && fighter.category.includes("KARATE COMBAT");
         });
-    }
-    catch (error){
-        console.error(error);
+        const grouped={};
+        filtered.forEach(fighter =>{
+            const weight = fighter.weightClass;
+            if(!grouped[weight]){
+                grouped[weight]=[]
+            }
+            grouped[weight].push(fighter);
+        });
+
+        container.innerHTML='';
+
+        for(const weightClass in grouped){
+            const fighterofClass = grouped[weightClass];
+
+            const title = document.createElement('h2');
+            title.textContent = weightClass;
+            container.appendChild(title);
+
+            const table = document.createElement('table');
+            table.innerHTML=`
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>Weight</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            `;
+            const tbody = table.querySelector('tbody');
+
+            fighterofClass.forEach(fighter => {
+                const row=document.createElement('tr');
+                row.innerHTML= `
+                    <td>${fighter.ranking ?? '-'}</td>
+                    <td>${fighter.fullName}</td>
+                    <td>${fighter.weightClass}</td>
+                `;
+                tbody.appendChild(row);
+            });
+            container.appendChild(table);
+        }
+        if(Object.keys(grouped).length ===0){
+            container.innerHTML='<p>No athletes</p>'
+        } 
+    }catch (error){
+        console.log(error);
     }
 }
 
-loadFighters()
+document.addEventListener('DOMContentLoaded', loadFighters);
